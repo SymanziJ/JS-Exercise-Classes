a51c588ab3a90ee7922fc61261fb0d2c856cb98f
SymanziJ <97126299+SymanziJ@users.noreply.github.com>
2022-01-20 21:09:58 -0500

    solved task 6, last commit actually only solved 4 and 5

--- index.js
+++ index.js
@@ -197,9 +197,20 @@ class Student extends Lambdasian {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
-   
+class ProjectManager extends Instructor {
+  constructor(atr){
+    super(atr);
+    this.gradClassName = atr.gradClassName;
+    this.favInstructor = atr.favInstructor;
+  }
+  standUp(channel){
+    return`${this.name} announces to ${channel}, @channel standy times!`;
+  }
+  debugsCode(studentOBJ, subject){
+    return `${this.name} debugs ${studentOBJ.name}'s code on ${subject}`;
+  }
 }
+
 /*
   STRETCH PROBLEM (no tests!)
     - Extend the functionality of the Student by adding a prop called grade and setting it equal to a number between 1-100.
